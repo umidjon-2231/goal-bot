b3222fb31d547ced4fd7d81f2b142cf383c0c2f6
umidjon-2231 <tumidjon808@gmail.com>
2024-03-07 08:26:47 +0500

    edit statistics

--- services/statisticService.js
+++ services/statisticService.js
@@ -5,7 +5,7 @@ const {ca, de} = require("date-fns/locale");
 
 const getStatistics = async (chatId, from, period, minus) => {
     let result = []
-    let goals = await goalService.getAllGoalByChatId(chatId).toArray();
+    let goals = await goalService.getAllGoalByChatId(chatId);
     let oldestGoal = await goalService.getOldestGoalOfChat(chatId);
 
     let {end, start} = timeService.periodToStartAndEndDate(period, minus, oldestGoal.createdTime);
