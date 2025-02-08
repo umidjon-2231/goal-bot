835417049f6bcfbc538cf926efaaebcdddf3b3be
umidjon-2231 <tumidjon808@gmail.com>
2025-02-09 00:41:04 +0500

    record history

--- services/timeService.ts
+++ services/timeService.ts
@@ -111,6 +111,7 @@ export const responsePeriodParser = (period: Period, minus: number): string => {
 
 
 export const parsePeriodFromDate= (date: Date, period: Period) => {
+    console.log(date);
     switch (period) {
         case "year":
             return format(date, `yyyy`);
